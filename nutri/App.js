fef1f5e48db67a53cd19cd430d968914dbd56e7e
vladimir.kryzhanovskyi <vladimir.kryzhanovskyi@digitalturbine.com>
2023-11-03 22:52:27 +0200

    landing

---
+++ nutri/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Дівчата! Всім привіт від мобільного додатку Соломійки!</Text>
+      <img src={require('/Users/vkryzha/Downloads/IMG_2212.HEIC')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
